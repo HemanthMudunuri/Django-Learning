7635b2b28175d8c04a5fa89678cbb9ef1af7e0b1
HemanthMudunuri <mudunuri.h@ekamobility.com>
2025-12-02 23:15:35 +0530

    Add Employee model, serializers, views, and API endpoints

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("students/", views.students),
     path("students/<int:pk>/", views.StudentDetailView),
+    # employee
+    path("employees/", views.Employees.as_view()),
+    path("employees/<int:pk>/", views.EmployeeDetail.as_view()),
 ]
